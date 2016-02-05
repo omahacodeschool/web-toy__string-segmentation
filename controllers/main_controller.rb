eabe25e88e3e5fde35014ba03113f03f9b473f2e
Chris Foral <chris.foral@gmail.com>
2016-02-05 16:45:08 -0600

    Tests Added

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -4,13 +4,18 @@ require_relative "../lib/string_segmenter"
 require_relative "../lib/dictionary"
 
 MyApp.get "/:string" do
+  
+  @test = StringSegmenter.new
+  
+  erb :"main/string_seg"
+end
+
+MyApp.get "/" do
   # If a GET request is made to the root path, the following line of code
   # looks for a .erb view file located in the 'views' directory at the given
   # location. So it's going to look for views/main/welcome.erb.
   # 
   # Then it will combine that view file with the layout file and sent the
   # combined document back to the client.
-  @test = StringSegmenter.new
-
   erb :"main/welcome"
 end
