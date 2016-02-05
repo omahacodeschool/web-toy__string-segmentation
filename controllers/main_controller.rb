4dec8b9f5eb588e84099eb5e6b3f7f35fbbdf7f7
Chris Foral <chris.foral@gmail.com>
2016-02-05 16:16:10 -0600

    It's a start

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -1,11 +1,16 @@
 # The path being defined for this controller action is just "/", meaning 
 # the root path. It's the homepage.
-MyApp.get "/" do
+require_relative "../lib/string_segmenter"
+require_relative "../lib/dictionary"
+
+MyApp.get "/:string" do
   # If a GET request is made to the root path, the following line of code
   # looks for a .erb view file located in the 'views' directory at the given
   # location. So it's going to look for views/main/welcome.erb.
   # 
   # Then it will combine that view file with the layout file and sent the
   # combined document back to the client.
+  @test = StringSegmenter.new
+
   erb :"main/welcome"
 end
---
+++ lib/string_segmenter.rb
@@ -0,0 +1,79 @@
+require_relative "dictionary.rb"
+
+class StringSegmenter
+
+  def initialize
+    @test_arr  = Array.new
+    @valid_arr = Array.new
+    @this_many = 1
+  end
+
+  def run_string_segmenter(orig_str)
+    @str = orig_str
+    @start_arr = @str.chars
+    empty_str?
+    until_empty_start_arr
+  end
+
+  def empty_str?
+    if @str.empty?
+      nil
+    end
+  end
+
+  def until_empty_start_arr
+    until @start_arr.empty?
+      start_arr_true
+      if test_arr_single
+      else
+        test_arr_nested
+        test_arr_dangler
+      end
+      @this_many = 1
+      @start_arr.shift(@valid_arr[-1].length.to_i)
+    end
+    @valid_arr
+  end  
+        
+  def start_arr_true
+    while @start_arr.length >= @this_many
+      str_section = @start_arr.take(@this_many).join
+
+      if valid_word?(str_section)
+        @test_arr << str_section
+      end  
+      @this_many += 1
+    end
+  end
+
+  def test_arr_single
+    if @test_arr.length == 1
+      @valid_arr << @test_arr.shift
+    end
+  end
+
+  def test_arr_nested
+    @this_many = 1
+    @str_loc = @test_arr[0].length
+
+    while @start_arr[@str_loc..-1].length >= @this_many
+      @new_section = @start_arr[@str_loc..-1].take(@this_many).join
+      
+      if valid_word?(@new_section)
+        @valid_arr << @test_arr.shift
+        @test_arr.clear
+        break
+      end
+
+      @this_many += 1
+    end
+  end
+
+  def test_arr_dangler       
+    if @test_arr.length > 0
+      @valid_arr << @test_arr.pop
+      @test_arr.clear
+      else
+    end
+  end
+end
