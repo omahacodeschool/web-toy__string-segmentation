d7db2d4cece9bf6ba8ae2d95870298c51ab75a61
Beth Haubert <bethany.haubert@gmail.com>
2016-02-05 09:42:36 -0600

    tests passing

--- tests/ss_test.rb
+++ tests/ss_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+
 class SSTest < Minitest::Test
   # These lines of code should not be edited. They add necessary
   # functionality for running our "acceptance tests".
@@ -13,7 +14,7 @@ class SSTest < Minitest::Test
 
     assert last_response.ok?
 
-    assert_includes last_response.body, "Welcome!"
+    assert_includes last_response.body, "magic word wizard"
   end
 
   def test_string
