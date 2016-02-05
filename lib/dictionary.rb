834cee75a8db1bc0b96d61a30f29b5df65ff7bda
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-05 14:23:10 -0600

    Struggle

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -3,7 +3,4 @@ def valid_word?(str)
   dictionary = ["turtle", "blue", "train", "trust", "smiles", "birth", "five", "smile", "asset", "as"]
 
   return dictionary.include?(str)
-
-  # Return true if your dictionary contains `word`
-  # or false if it does not.
 end
