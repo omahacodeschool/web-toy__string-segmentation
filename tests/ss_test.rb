696678e35cac810f121cb9ccf767b57efc91e4c6
Amy Wall <amymariewall@gmail.com>
2016-02-07 20:58:10 -0600

    String Segmenter with Tests

--- tests/ss_test.rb
+++ tests/ss_test.rb
@@ -8,6 +8,22 @@ class SSTest < Minitest::Test
   end
 
   # The actual tests go below this comment.
+
+  def test_show_words
+    get '/wafflepuzzleblues'
+
+    assert last_response.ok?
+
+    assert_includes last_response.body, "waffle, puzzle, blues"
+  end
+
+   def test_show_words
+    get '/tigerssharkslions'
+
+    assert last_response.ok?
+
+    assert_includes last_response.body, "tigers, sharks, lions"
+  end
   
 end
 
